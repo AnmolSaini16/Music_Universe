1d6aa0957a2468709af13ae273e24f0b109c01d6
Anmoldeep Singh <anmoldeep.singh@optym.com>
2022-11-22 16:53:06 +0530

    CHANGES

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,28 +47,23 @@ export default HomePage;
 
 export async function getServerSideProps(context: NextPageContext) {
   try {
-    const session = await getSession({ req: context.req });
-    console.log(session?.accessToken);
-    if (!session?.user || session.error === "RefreshAccessTokenError") {
-      return {
-        redirect: {
-          destination: "/auth/login",
-          permanent: false,
-        },
-      };
-    }
-    const featuredplaylists = await _featuredPlayLists(
-      session?.accessToken as string
-    );
+    // const session = await getSession({ req: context.req });
+    // console.log(session?.accessToken);
+    const token =
+      "BQDhxur5Hx5lSpqPyxupzSeej2x7vvhpDOKHl0C-VYmcy80P-P-N6cdPokbWQFnG7Yt3TKsJd1DpPkKBNJdanBozuAARqiYUrUwYviXmOIimnNECIn9Ml5Ec90aWPhZbpY41KeSzpdon_L-5QB9ogosV9l4OXRReXTmzBHbc13w1hJLD6pPlYvr0--YWyMpQkFVS5aA";
+    // if (!session?.user || session.error === "RefreshAccessTokenError") {
+    //   return {
+    //     redirect: {
+    //       destination: "/auth/login",
+    //       permanent: false,
+    //     },
+    //   };
+    // }
+    const featuredplaylists = await _featuredPlayLists(token);
 
-    const currentUserPlayList = await _currentUserPlayList(
-      session?.accessToken as string
-    );
+    const currentUserPlayList = await _currentUserPlayList(token);
 
-    const getServeralCategories = await _getCategories(
-      session?.accessToken as string,
-      14
-    );
+    const getServeralCategories = await _getCategories(token, 14);
 
     return {
       props: {
@@ -83,12 +78,12 @@ export async function getServerSideProps(context: NextPageContext) {
     //@ts-ignore
     if (error.response.status === 401) {
       //// Unauthorised
-      return {
-        redirect: {
-          destination: "/auth/login",
-          permanent: false,
-        },
-      };
+      // return {
+      //   redirect: {
+      //     destination: "/auth/login",
+      //     permanent: false,
+      //   },
+      // };
     }
     return {
       props: {},
